2a207d071797d012ecbd2c09f0242fafa89cce07
dsabirbaev <davran9559@gmail.com>
2024-01-10 17:49:45 +0500

    add project bozor

---
+++ src/components/MinHeader/MinHeader.vue
@@ -0,0 +1,25 @@
+<template>
+    
+     <div class="container">
+        <div class="flex items-center justify-between">
+            <div class="">
+                left
+                
+            </div>
+            <div>
+                right
+            </div>
+        </div>
+     </div>
+    
+</template>
+
+<script setup>
+
+import Button from 'primevue/button';
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/pages/Layout.vue
@@ -0,0 +1,24 @@
+
+
+
+<template>
+    <MinHeader />
+    <Header />
+    <main>
+        <router-view> </router-view>
+    </main>
+    <Footer  />
+</template>
+
+
+
+<script setup>
+   import MinHeader from "@cmp/MinHeader/MinHeader.vue";
+   import Header from "@cmp/Header/Header.vue";
+   import Footer from "@cmp/Footer/Footer.vue";
+</script>
+
+
+<style lang="scss" scoped>
+
+</style>
