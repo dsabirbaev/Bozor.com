3a0d9e7cce8980f19b33b17989a99371145eb8ed
dsabirbaev <davran9559@gmail.com>
2024-01-11 00:20:13 +0500

    add header

--- src/components/Header/Header.vue
+++ src/components/Header/Header.vue
@@ -1,7 +1,40 @@
 <template>
-    <header>
+    <header class="bg-white">
      <div class="container">
-        Header
+        <div class="flex items-center justify-between h-[78px]">
+            <a href="/" title="Bosh sahifa">
+                <img src="https://bozor.com/_nuxt/img/logo.a6ecee7.webp" alt="Bozo.com logo">
+            </a>
+
+            <div class="flex items-center justify-between">
+                <a href="/catalog" title="Katalog" class="text-[12px] text-[#2B4FAB] flex items-center gap-x-1 py-[13px] px-[24px] rounded-md border-solid border-2 border-gray-100 font-bold">
+                    <span class="pi pi-th-large"></span> 
+                    <span>Katalog</span>
+                </a>
+            </div>
+
+            <div class="flex items-center rounded-md bg-[#F5F7FA] p-[16px] w-[288px]">
+                <span class="pi pi-search"></span>
+                <input class="placeholder:text-gray-400 ml-[16px] border-none outline-none bg-transparent" type="text" placeholder="Mahsulotlarni topish" autocomplete="off">
+            </div>
+
+            <div class="flex items-center gap-x-1 text-[12px] rounded-md bg-[#F5F7FA] py-[16px] px-[13px]">
+                <p class="text-[#2B4FAB]">UZS</p>
+                <span class="pi pi-angle-down text-[#2B4FAB]"></span>
+            </div>
+
+            <a href="/favorites" title="Saralanganlar" class="rounded-md bg-[#F5F7FA] py-[16px] px-[13px]">
+                <span class="pi pi-heart text-[18px]"></span>
+            </a>
+
+            <a href="/basket" title="Savat" class="rounded-md bg-[#F5F7FA] py-[16px] px-[13px]">
+                <span class="pi pi-shopping-cart text-[18px]"></span>
+            </a>
+
+            <button title="Profil" class="rounded-md border-none bg-[#F5F7FA] py-[16px] px-[13px]">
+                <span class="pi pi-user text-[18px]"></span>
+            </button>
+        </div>
      </div>
     </header>
 </template>
--- src/components/MinHeader/MinHeader.vue
+++ src/components/MinHeader/MinHeader.vue
@@ -1,22 +1,48 @@
 <template>
     
-     <div class="container">
-        <div class="flex items-center justify-between">
-            <div class="">
-                left
+     <header class="bg-[#F5F7FA]">
+        <div class="container">
+            <div class="flex items-center justify-between h-[40px]">
+                <div class="flex items-center space-x-1 text-[12px]">
+                    <span class="pi pi-map-marker text-gray-500 text-[16px]"></span>
+                    <span>Manzil: </span>
+                    <p>Toshkent</p>
+                </div>
+                <div class="flex items-center text-[12px] gap-x-5">
+                    
+                        
+                        <a class="flex items-center gap-x-[6px]" href="https://t.me/bozor_com" target="_blank" title="Bozor.com telegram">
+                            <span class="pi pi-telegram text-blue-500 text-[16px]"></span>
+                            <p>t.me/bozor_com</p>
+                        </a>
+
+                        <a class="flex items-center gap-x-[6px]" title="+998 95 515 65 15" href="tel:+998 95 515 65 15">
+                            <span class="pi pi-phone text-[16px]"></span> 
+                            <p>+998 95 515 65 15</p>
+                        </a>
+
+                        <Dropdown v-model="selectedCity" :options="cities" optionLabel="name" placeholder="Uzb" class="w-fit text-[14px]" />
+                </div>
+                    
                 
             </div>
-            <div>
-                right
-            </div>
-        </div>
      </div>
+     </header>
     
 </template>
 
 <script setup>
 
-import Button from 'primevue/button';
+
+import Dropdown from 'primevue/dropdown';
+
+import { ref } from "vue";
+
+const selectedCity = ref();
+const cities = ref([
+    { name: 'Рус', code: 'RU' },
+    { name: 'Uzb', code: 'UZ' }
+]);
 
 </script>
 
