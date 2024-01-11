d48f3a310b47d0de48309b52fd11d525056b6ff8
dsabirbaev <davran9559@gmail.com>
2024-01-11 12:04:01 +0500

    add intro

---
+++ src/components/Intro/Intro.vue
@@ -0,0 +1,35 @@
+<template>
+    <section>
+        <div class="container">
+            <Carousel class="intro-carousel" :value="images" :numVisible="1" :numScroll="1" :autoplayInterval="3000" indicatorButton="false">
+                <template #item="slotProps">
+                    <div class="carousel-data rounded-md relative">
+                        <img :src="slotProps.data.src" class="w-full h-[250px] rounded-xl ">
+                    </div>
+                </template>
+            </Carousel>
+        </div>
+    </section>
+</template>
+
+<script setup>
+
+
+
+    import { ref } from 'vue';
+    import Carousel from 'primevue/carousel';
+    const images = ref([
+        { src: new URL('@ass/images/clear.png', import.meta.url) },
+        { src: new URL('@ass/images/zateya.jpeg', import.meta.url) },
+        { src: new URL('@ass/images/alpen.webp', import.meta.url) },
+        
+    ]);
+
+    
+</script>
+
+<style lang="scss">
+    .p-carousel-indicators{
+        display: none;
+    }
+</style>
--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,7 @@ export default defineConfig({
             {find: "@", replacement: fileURLToPath(new URL("./src", import.meta.url))},
             {find: "@cmp", replacement: fileURLToPath(new URL("./src/components", import.meta.url))},
             {find: "@pg", replacement: fileURLToPath(new URL("./src/pages", import.meta.url))},
+            {find: "@ass", replacement: fileURLToPath(new URL("./src/assets", import.meta.url))},
         ],
     },
 });
