bf80d2a36ab9f39d8baf67a45007f52475dff862
dsabirbaev <davran9559@gmail.com>
2024-01-12 12:04:45 +0500

    add category section

---
+++ src/components/Catalog/Catalog.vue
@@ -0,0 +1,168 @@
+<template>
+    <section>
+        <div class="container">
+            <div class="flex justify-between mb-4">
+                <div class="rounded-[16px] bg-white min-w-[377px] px-5 py-[18px]">
+                    <h2 class="text-[18px] font-['TTInterfaceSemiBold'] mb-5">Oziq ovqat maxsulotlari</h2>
+                    <div class="mb-5">
+                        <div class="flex justify-between mb-3">
+                            <a href="/catalog/yo-masulotlari" class="" title="YOG‘ MAHSULOTLARI">
+                                <img class="rounded-[16px] w-[160px] h-[128px]" src="https://api.bozor.com/media/cache/8c/00/8c00d787510cf2cb06e25c58e30bdbd4.webp" alt="YOG‘ MAHSULOTLARI" style="background-color: rgb(239, 238, 237);"> 
+                                <h3 class="text-[12px]  text-[#020105] font-['TTInterfaceLight'] mt-2">Yog' mahsulotlari</h3>
+                            </a>
+                            <a href="/catalog/ketchuplarsouslar-mayonez" class="" title="KETCHUP, MAYONEZ, SOUS, SIRKA">
+                                <img class="rounded-[16px] w-[160px] h-[128px] object-contain" src="https://api.bozor.com/media/cache/0f/49/0f4982c2be2460115c57beb1ca931127.webp" alt="KETCHUP, MAYONEZ, SOUS, SIRKA" style="background-color: rgb(239, 238, 237);"> 
+                                <h3 class="text-[12px]  text-[#020105] font-['TTInterfaceLight'] mt-2">Ketchup, mayonez, sous, sirka</h3>
+                            </a>
+                        </div>
+                       
+                       <div class="flex justify-between mb-3">
+                            <a href="/catalog/suvichimliklar" class="" title="SUV/ICHIMLIKLAR">
+                                <img class="rounded-[16px] w-[160px] h-[128px] object-contain" src="https://api.bozor.com/media/cache/70/58/70581fec250d97b2ce5f93c435db815d.webp" alt="SUV/ICHIMLIKLAR" style="background-color: rgb(239, 238, 237);"> 
+                                <h3 class="text-[12px]  text-[#020105] font-['TTInterfaceLight'] mt-2">Suv/ichimliklar</h3>
+                            </a>
+                            <a href="/catalog/makaron" class="" title="MAKARON, SPAGETTI VA LAPSHA">
+                                <img class="rounded-[16px] w-[160px] h-[128px] object-contain" src="https://api.bozor.com/media/cache/09/25/09250f58f7cec7cccda85133b1ce45ad.webp" alt="MAKARON, SPAGETTI VA LAPSHA" style="background-color: rgb(239, 238, 237);"> 
+                                <h3 class="text-[12px]  text-[#020105] font-['TTInterfaceLight'] mt-2">Makaron, spagetti va lapsha</h3>
+                            </a>
+                       </div>
+                    </div>
+
+                    <a href="/catalog?show=ozi-ovat-masulotlari" class="text-[#2b4fab] underline text-[14px] font-['TTInterfaceMedium']" title="OZIQ OVQAT MAXSULOTLARI">
+                        Ko'proq
+                    </a>
+                </div>
+
+                <div class="rounded-[16px] bg-white min-w-[377px] px-5 py-[18px]">
+                    <h2 class="text-[18px] font-['TTInterfaceSemiBold'] mb-5">Maishiy mahsulotlar</h2>
+                    <div class="mb-5">
+                        <div class="flex justify-between mb-3">
+                            <a href="/catalog/yo-masulotlari" class="" title="Kantselyariya mahsulotlari">
+                                <img class="rounded-[16px] w-[160px] h-[128px]" src="https://api.bozor.com/media/cache/0e/60/0e60be49899558949948333fb0d47977.webp" alt="Kantselyariya mahsulotlari" style="background-color: rgb(239, 238, 237);"> 
+                                <h3 class="text-[12px]  text-[#020105] font-['TTInterfaceLight'] mt-2">Kantselyariya mahsulotlari</h3>
+                            </a>
+                            <a href="/catalog/ketchuplarsouslar-mayonez" class="" title="Gigiena">
+                                <img class="rounded-[16px] w-[160px] h-[128px] object-contain" src="https://api.bozor.com/media/cache/a7/cd/a7cdd0e9436e36f7430403a137bf689e.webp" alt="Gigiena" style="background-color: rgb(239, 238, 237);"> 
+                                <h3 class="text-[12px]  text-[#020105] font-['TTInterfaceLight'] mt-2">Gigiena</h3>
+                            </a>
+                        </div>
+                       
+                       <div class="flex justify-between mb-3">
+                            <a href="/catalog/suvichimliklar" class="" title="Yuvish vositalari">
+                                <img class="rounded-[16px] w-[160px] h-[128px] object-contain" src="https://api.bozor.com/media/cache/90/8e/908ea08aebcc0aad31d1c38a9ff550cf.webp" alt="Yuvish vositalari" style="background-color: rgb(239, 238, 237);"> 
+                                <h3 class="text-[12px]  text-[#020105] font-['TTInterfaceLight'] mt-2">Yuvish vositalari</h3>
+                            </a>
+                            <a href="/catalog/makaron" class="" title="Ho'jalik mollari">
+                                <img class="rounded-[16px] w-[160px] h-[128px] object-contain" src="https://api.bozor.com/media/cache/92/9b/929b19ad85f5b1c31a2b85746400bc11.webp" alt="Ho'jalik mollari" style="background-color: rgb(239, 238, 237);"> 
+                                <h3 class="text-[12px]  text-[#020105] font-['TTInterfaceLight'] mt-2">Ho'jalik mollari</h3>
+                            </a>
+                       </div>
+                    </div>
+
+                    <a href="/catalog?show=ozi-ovat-masulotlari" class="text-[#2b4fab] underline text-[14px] font-['TTInterfaceMedium']" title="OZIQ OVQAT MAXSULOTLARI">
+                        Ko'proq
+                    </a>
+                </div>
+
+                <div class="rounded-[16px] bg-white min-w-[377px] px-5 py-[18px]">
+                    <h2 class="text-[18px] font-['TTInterfaceSemiBold'] mb-5">Maishiy texnika</h2>
+                    <div class="mb-5">
+                        <div class="flex justify-between mb-3">
+                            <a href="/catalog/yo-masulotlari" class="" title="Konditsionerlar">
+                                <img class="rounded-[16px] w-[160px] h-[128px]" src="https://api.bozor.com/media/cache/49/5c/495ca97d14e6351374af7caaba3bcbc3.webp" alt="Konditsionerlar" style="background-color: rgb(239, 238, 237);"> 
+                                <h3 class="text-[12px]  text-[#020105] font-['TTInterfaceLight'] mt-2">Konditsionerlar</h3>
+                            </a>
+                            <a href="/catalog/ketchuplarsouslar-mayonez" class="" title="Muzlatgichlar">
+                                <img class="rounded-[16px] w-[160px] h-[128px] object-contain" src="https://api.bozor.com/media/cache/8c/49/8c49e6f32c6a2c0f6dd87a5903171b3f.webp" alt="Muzlatgichlar" style="background-color: rgb(239, 238, 237);"> 
+                                <h3 class="text-[12px]  text-[#020105] font-['TTInterfaceLight'] mt-2">Muzlatgichlar</h3>
+                            </a>
+                        </div>
+                       
+                       <div class="flex justify-between mb-3">
+                            <a href="/catalog/suvichimliklar" class="" title="Elektrli va mikroto'lqinli pechlar">
+                                <img class="rounded-[16px] w-[160px] h-[128px] object-contain" src="https://api.bozor.com/media/cache/e0/bf/e0bf03452ca6580ef0c1583b2b3df021.webp" alt="Elektrli va mikroto'lqinli pechlar" style="background-color: rgb(239, 238, 237);"> 
+                                <h3 class="text-[12px]  text-[#020105] font-['TTInterfaceLight'] mt-2">Elektrli va mikroto'lqinli pechlar</h3>
+                            </a>
+                            <a href="/catalog/makaron" class="" title="Kir yuvish mashinalari">
+                                <img class="rounded-[16px] w-[160px] h-[128px] object-contain" src="https://api.bozor.com/media/cache/1d/4a/1d4a24028d4ef951962a4d125bf037d5.webp" alt="Kir yuvish mashinalari" style="background-color: rgb(239, 238, 237);"> 
+                                <h3 class="text-[12px]  text-[#020105] font-['TTInterfaceLight'] mt-2">Kir yuvish mashinalari</h3>
+                            </a>
+                       </div>
+                    </div>
+
+                    <a href="/catalog?show=ozi-ovat-masulotlari" class="text-[#2b4fab] underline text-[14px] font-['TTInterfaceMedium']" title="OZIQ OVQAT MAXSULOTLARI">
+                        Ko'proq
+                    </a>
+                </div>
+            </div>
+
+            <div class="flex justify-between">
+                <div class="rounded-[16px] bg-white min-w-[377px] px-5 py-[18px]">
+                    <h2 class="text-[18px] font-['TTInterfaceSemiBold'] mb-5">Televizor, foto-video va audio</h2>
+                    <div class="mb-[26px]">
+                        <a href="/catalog?show=televizor-foto-video-va-audio" class="" title="Televizor, foto-video va audio">
+                            <img src="https://api.bozor.com/media/tv-1.webp" alt="Televizor, foto-video va audio" class="w-[335px] h-[308px] rounded-[16px]">
+                        </a>
+                    </div>
+
+                    <a href="/catalog?show=ozi-ovat-masulotlari" class="text-[#2b4fab] underline text-[14px] font-['TTInterfaceMedium']" title="OZIQ OVQAT MAXSULOTLARI">
+                        Ko'proq
+                    </a>
+                </div>
+
+                <div class="rounded-[16px] bg-white min-w-[377px] px-5 py-[18px]">
+                    <h2 class="text-[18px] font-['TTInterfaceSemiBold'] mb-5">Smartfon, telefon, gadjet, aksesuarlar</h2>
+                    <div class="mb-5">
+                        <div class="flex justify-between mb-3">
+                            <a href="/catalog/yo-masulotlari" class="" title="Radiotelefonlar">
+                                <img class="rounded-[16px] w-[160px] h-[128px]" src="https://api.bozor.com/media/cache/07/1a/071a1e8cec1edc0ec8f59819649818ac.png" alt="Radiotelefonlar" style="background-color: rgb(239, 238, 237);"> 
+                                <h3 class="text-[12px]  text-[#020105] font-['TTInterfaceLight'] mt-2">Radiotelefonlar</h3>
+                            </a>
+                            <a href="/catalog/ketchuplarsouslar-mayonez" class="" title="Telefonlar">
+                                <img class="rounded-[16px] w-[160px] h-[128px] object-contain" src="https://api.bozor.com/media/cache/c9/c6/c9c673221bc244c4b17f46a574bcee42.png" alt="Telefonlar" style="background-color: rgb(239, 238, 237);"> 
+                                <h3 class="text-[12px]  text-[#020105] font-['TTInterfaceLight'] mt-2">Telefonlar</h3>
+                            </a>
+                        </div>
+                       
+                       <div class="flex justify-between mb-3">
+                            <a href="/catalog/suvichimliklar" class="" title="Planshetlar">
+                                <img class="rounded-[16px] w-[160px] h-[128px] object-contain" src="https://api.bozor.com/media/cache/a7/59/a7591063d640397a726b503d88752e1e.png" alt="Planshetlar" style="background-color: rgb(239, 238, 237);"> 
+                                <h3 class="text-[12px]  text-[#020105] font-['TTInterfaceLight'] mt-2">Planshetlar</h3>
+                            </a>
+                            <a href="/catalog/makaron" class="" title="Smartfonlar">
+                                <img class="rounded-[16px] w-[160px] h-[128px] object-contain" src="https://api.bozor.com/media/cache/88/b3/88b3f9d881e06aef6be7857228a4b36f.png" alt="Smartfonlar" style="background-color: rgb(239, 238, 237);"> 
+                                <h3 class="text-[12px]  text-[#020105] font-['TTInterfaceLight'] mt-2">Smartfonlar</h3>
+                            </a>
+                       </div>
+                    </div>
+
+                    <a href="/catalog?show=ozi-ovat-masulotlari" class="text-[#2b4fab] underline text-[14px] font-['TTInterfaceMedium']" title="OZIQ OVQAT MAXSULOTLARI">
+                        Ko'proq
+                    </a>
+                </div>
+
+                <div class="rounded-[16px] bg-white min-w-[377px] px-5 py-[18px]">
+                    <h2 class="text-[18px] font-['TTInterfaceSemiBold'] mb-5">Kompyuter qurilmalari</h2>
+                    <div class="mb-[26px]">
+                        <a href="/catalog?show=televizor-foto-video-va-audio" class="" title="Kompyuter qurilmalari">
+                            <img src="https://api.bozor.com/media/450b51c6db2528f9dfe1c6c1172f1716.webp" alt="Kompyuter qurilmalari" class="w-[335px] h-[308px] rounded-[16px]">
+                        </a>
+                    </div>
+
+                    <a href="/catalog?show=ozi-ovat-masulotlari" class="text-[#2b4fab] underline text-[14px] font-['TTInterfaceMedium']" title="OZIQ OVQAT MAXSULOTLARI">
+                        Ko'proq
+                    </a>
+                </div>
+            </div>
+        </div>
+    </section>
+
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/pages/Home/Home.vue
+++ src/pages/Home/Home.vue
@@ -4,12 +4,15 @@
 
 <template>
    <Intro/>
+   <Catalog/>
    <Brands/>
+   
 </template>
   
 <script setup>
     import Intro from "@cmp/Intro/Intro.vue";
     import Brands from "@cmp/Brands/Brands.vue";
+    import Catalog from "@cmp/Catalog/Catalog.vue";
 </script>
   
 <style lang="scss" scoped></style>
