c6b385e4c1e5a6406ca166f5115af6a89b428d60
dsabirbaev <davran9559@gmail.com>
2024-01-14 22:41:57 +0500

    add dropdown menu

--- src/components/Header/Header.vue
+++ src/components/Header/Header.vue
@@ -23,10 +23,25 @@
                 <span>{{ $t('manzilini kiriting') }}</span>
                 <AddressModal :visibleAddress="visibleAddress" @closeModalAddress="closeModalAddress" />
             </button>
-            <div class="flex items-center gap-x-1 text-[12px] rounded-md bg-[#F5F7FA] h-[45px] px-[13px]">
-                <p class="text-[#2B4FAB]">UZS</p>
-                <span class="pi pi-angle-down text-[#2B4FAB]"></span>
+
+            <div class="dropdown relative flex items-center gap-x-1 text-[12px] rounded-md bg-[#F5F7FA] h-[45px] px-[13px] cursor-pointer">
+                <p class="text-[#2B4FAB] font-['TTInterfaceMedium']">UZD</p>
+                <span class="pi pi-angle-down text-[#d2d9eb]"></span>
+
+                <div class="dropdown-menu  flex flex-col gap-y-5 absolute top-full left-[-20px] z-20 p-4 rounded-md w-[170px] bg-white">
+                    <p class="text-[16px] text-[#020105] font-['TTInterfaceSemiBold']">Valyutani tanlang</p>
+                    <button class="flex items-center gap-x-3 bg-transparent border-none outline-none cursor-pointer">
+                        <img src="data:image/webp;base64,UklGRlQBAABXRUJQVlA4WAoAAAAQAAAAIwAAFAAAQUxQSBcAAAABD/DA/4iIQJBtc4P501bgABH9T2jvEABWUDggFgEAAPAFAJ0BKiQAFQA+USaQRiOiIaElaABwCglmAMovXWzVsmgiHudA9QH6Vgua/xb885LEDoqJZKhoAP7xlrvARClwvz/kAtrV7D8b25q6Ldti/8e/+UxSt7M2BQ3bPM5BX/BYsvnS9Xt0wpp/Z2cGgHdUf2t/9g364f84AlfvL7spOLVdU8HWqKIDgVQNkerd3pXfXPr5SUxKV+G/50Of/AOOXZLhFF/7Uj4F/mnE9YIg5KD+5fsBP/6fw7xwNs4eI/8G4rOx+tSj+JzRbzRFiubv0A9bbAIAZhe/MezQN6mPfBkJ8MyP7bgwYwf82EX/90V/009uJqBT9JnZY7f/lt/Zl1R/N5dj+VH/7nA/jYkxlUFggAAA" width="25" height="15" alt="Usd"> 
+                        <p class="text-[14px] font-['TTInterfaceMedium']">USD</p>
+                    </button>
+                    <button class="flex items-center gap-x-3 bg-transparent border-none outline-none cursor-pointer">
+                        <img src="data:image/webp;base64,UklGRhABAABXRUJQVlA4WAoAAAAQAAAAIwAAGAAAQUxQSBcAAAABD/DA/4iIQJhtdAbzpy3B/SP6n9AeIQBWUDgg0gAAAPAFAJ0BKiQAGQA+USaQRiOiIaElaABwCglqAMURAD8ANMB+AH5ABGyA0gr6KFoQh2fjxGDJtJJAAPw3BgdCTPIQaD0BU2Z6Zu0346yL9Fvs5gIPF+n6YNY4aOGOb4e3H/1vt/8Hqb8+bCNd7Ppkp//8Bp/X/US/Q0CPSPg2E4Y9hXCx60JA1MOprI99V16dTXiTWXn6frqzuiYTLYvy5pOEAH0AEzFkAOoLSisYkZUzCE5079gDQb7e/o5MVH/VU8+/scmKj+HKf8OBfAbWVWwAAA==" width="25" height="15" alt="Uzs"> 
+                        <p class="text-[14px] font-['TTInterfaceMedium']">UZS</p>
+                    </button>
+                </div>
             </div>
+
+            
 
             <router-link to="/favorites" title="Saralanganlar" class="flex items-center rounded-md bg-[#F5F7FA] h-[45px] px-[13px]">
                 <span class="pi pi-heart text-[18px]"></span>
@@ -51,6 +66,8 @@
     import { ref, onMounted, onBeforeUnmount } from 'vue';
     import {ProfilModal, AddressModal} from "@cmp/UI/Modal";
     
+    import Dropdown from 'primevue/dropdown';
+
     /// profile modal
     const visible = ref(false);
     const openModal = () => {
@@ -95,5 +112,13 @@
         top: 0;
         width: 100%;
         
+    }
+
+    .dropdown-menu {
+        display: none;
+    }
+    .dropdown:hover .dropdown-menu {
+       
+        display: flex;
     }
 </style>
