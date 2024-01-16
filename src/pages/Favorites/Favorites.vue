09cbd1137e730eef9590fa9a461149fddbb55dec
dsabirbaev <davran9559@gmail.com>
2024-01-16 18:29:46 +0500

    add trash in favorites

--- src/pages/Favorites/Favorites.vue
+++ src/pages/Favorites/Favorites.vue
@@ -2,7 +2,7 @@
 
 <template>
     <section class="pt-10">
-        <div class="container">
+        <div class="container p-[12px]">
             <div class="catalog-banner h-[185px] flex items-center justify-center flex-col mb-10">
                 <router-link to="/" class="text-white text-[14px] font-['TTInterfaceMedium']" title="Bosh sahifa">
                     {{ $t('bosh sahifa') }}
@@ -10,12 +10,24 @@
                 <h1 class="text-white font-['TTInterfaceBold'] text-[35px] my-[16px]">{{ $t('saralanganlar') }}</h1>
                 <p class="text-[16px] text-white font-['TTInterfaceSemiBold']">0 {{ $t('mahsulot') }}</p>
             </div>
+
+            <div>
+                <div class="flex items-center justify-between mb-5">
+                    <div class="text-[14px] text-[#020105] font-['TTInterfaceSemiBold'] p-[14px] w-[200px] flex items-center justify-center cursor-pointer bg-white rounded-[12px]">
+                        Kategoriyalarni yashirish
+                    </div>
+                    <div class="flex items-center gap-x-2 cursor-pointer text-[#e45746]">
+                        <span class="pi pi-trash text-[16px]"></span>
+                        <span class="font-['TTInterfaceMedium'] text-[14px]">Tozalash</span>
+                    </div>
+                </div>
+            </div>
         </div>
     </section>
 </template>
   
 <script setup>
-     import { useTitle } from '@vueuse/core'
+    import { useTitle } from '@vueuse/core'
 
     const title = useTitle()
     title.value = "Bozor.com | Saralanganlar"
